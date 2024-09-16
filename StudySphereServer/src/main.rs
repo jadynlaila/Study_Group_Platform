1afc9604fac17d3320d99cc219652be172440fe9
Alexander Anthis <anthisalex@gmail.com>
2024-09-15 18:14:39 -0700

    Added some basic networking

--- StudySphereServer/src/main.rs
+++ StudySphereServer/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+mod api;
+
+use api::task::{
+    get_task
+}; // more will be added here later
+
+use actix_web::{
+    HttpServer,
+    App,
+    
+};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
+
+    HttpServer::new(move || {
+        let logger = Logger::default();
+        App::new()
+        .wrap(logger)
+        .app_data()
+        .service(get_task)
+    })
 }
