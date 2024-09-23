f274e6cb4d34f814463eab9a6d1e13e0a580aa5d
Alexander Anthis <anthisalex@gmail.com>
2024-09-22 20:54:42 -0700

    Initial test server setup

---
+++ backend_server/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+var app = express();
+
+app.get("/", function(request, response) {
+    response.send("Hello world!");
+})
+
+app.listen(10000, function() {
+    console.log("Started application on port %d", 10000);
+})
