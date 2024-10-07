69ad65727feaa162d106c89739a213af6cd687a6
Alexander Anthis <aaa2489@nau.edu>
2024-10-07 16:22:07 -0700

    Mongo Setup (#60)
    * Removed node_modules directory
    
    * Added initial js controller and outline
    
    * Updated some minor files
    
    * Schemas created
    
    * Finished schemas
    
    * updating schemas- error with put request
    
    * Connected to mongoDB- get, set, update, and delete 'goals'
    
    * Updated controller and router to be relevant for student info
    
    ---------
    
    Co-authored-by: Jadyn Calhoun <jlc2243@nau.edu>

---
+++ backend/models/MessageModel.js
@@ -0,0 +1,30 @@
+// Messages
+// Time stamp
+// User who sent it
+
+
+const mongoose = require('mongoose');
+
+const MessageSchema = new Schema ({
+    //! keep in mind:
+    //! messages cannot be sent while empty
+    //! and we need a character limit
+    //! this will need to be addressed in the 'send message' function
+    content: {
+        type: String
+    },
+    time: {
+        type: Date,
+        value: Date,
+        required: [true, 'message needs date']
+    },
+    author: {
+        type: Schema.Types.ObjectId, 
+        ref: "Student"
+    }
+},
+{
+    timestamps: true
+})
+
+module.exports = mongoose.model("Message", MessageSchema)
