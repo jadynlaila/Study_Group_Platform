aa93850cd6681e73e3cf4d432bdda2ee284c15e0
Alexander Anthis <anthisalex@gmail.com>
2024-10-09 07:09:40 -0700

    Added some setters and functions

--- backend/controllers/groupController.js
+++ backend/controllers/groupController.js
@@ -1,10 +1,11 @@
 
 // Requires //
-
+const mongoose = require("mongoose")
 
 // Global Variables //
 
 
+// Classes //
 class Group {
     constructor(name, description, courses, majors,
         memberLimit, memberCount, ownerID, 
@@ -24,5 +25,54 @@ class Group {
         this.messageIDs = messageIDs;
     }
 
-    
+    // Getters //
+
+
+    // Setters //
+
+    set name(value) {
+        if (!value.isWellFormed()) {
+            throw new Error("Name contains invalid characters");
+        }
+        this.name = value;
+    }
+
+    set description(value) {
+        if (!value.isWellFormed()) {
+            throw new Error("Description contains invalid characters");
+        }
+        this.description = value;
+    }
+
+    set memberCount(value) {
+        if (value > this.memberLimit) {
+            throw new Error(`Member count ${value} is bigger than the current limit of ${this.memberLimit}`);
+        }
+        this.memberCount = value;
+    }
+
+    set ownerID(value) {
+        if (!userIsReal(value)) {
+            throw new Error(`User "${value}" could not be found!`);
+        }
+        this.ownerID = value;
+    }
+}
+
+
+function findUserFromID(userID) {
+    return mongoose.findById(userID);
+}
+
+
+function findUserFromUsername(username) {
+    return mongoose.findOne({ username: `${username}` });
+}
+
+
+function userIsReal(userID) {
+    if (!findUserFromID(userID)) {
+        return false;
+    }
+    return true;
 }
