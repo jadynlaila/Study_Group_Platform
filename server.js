a7bd77e3136f00b235c373c957b84ee344d101c2
Alexander Anthis <anthisalex@gmail.com>
2024-10-27 13:51:55 -0700

    Launch react subprocess in another process

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./backend/middleware/errorMiddleware")
+const { protectRoute } = require("./backend/middleware/protectRoute")
+const connectDB = require("./backend/config/db");
+const cookieParser = require("cookie-parser");
+const { spawn } = require("child_process");
+
+const port = process.env.PORT || 6789;
+
+const app = express()
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser());
+
+app.use("/api/group", require("./backend/routes/groupRoutes"))
+app.use("/api/student", require("./backend/routes/studentRoutes"))
+app.use("/api/messages", require("./backend/routes/messageRoutes"));
+app.use(errorHandler)
+
+const reactServer = spawn("npm", ["start"], {
+    cwd: "frontend",
+    stdio: "inherit"
+})
+
+reactServer.on("close", (code) => {
+    console.log(`React server exited with code ${code}`)
+})
+
+app.listen(port, () => console.log(`Server has started on port ${port}`))
