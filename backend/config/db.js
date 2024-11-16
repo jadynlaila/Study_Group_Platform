4c1f91fce80a99ac59fc442f69a50803c8fbaca7
Alexander Anthis <anthisalex@gmail.com>
2024-11-15 19:47:32 -0700

    moved mongo details out of single URI var

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,17 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
-    console.log(`\x1b[32mUsing database '${process.env.DATABASE}'\x1b[0m`)
+    const MONGO_URI = process.env.MONGO_URI
+    const MONGO_USERNAME = process.env.MONGO_USERNAME
+    const MONGO_PASSWORD = process.env.MONGO_PASSWORD
+    const MONGO_DATABASE = process.env.MONGO_DATABASE
+    const MONGO_CONN_DETAILS = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_URI}/${process.env.MONGO_DATABASE}`
+
+    console.log(`\x1b[32mConnecting to Mongo as '${MONGO_USERNAME}'\x1b[0m`)
+    console.log(`\x1b[32mUsing database '${MONGO_DATABASE}'\x1b[0m`)
 
     try {
-        const conn = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DATABASE}`);
+        const conn = await mongoose.connect(MONGO_CONN_DETAILS);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
