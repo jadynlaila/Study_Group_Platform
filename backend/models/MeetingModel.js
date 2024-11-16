c02efe5cec8e800d41b200d894f0fd0800b36e24
Alexander Anthis <anthisalex@gmail.com>
2024-11-16 09:12:34 -0700

    Created the initial data model

---
+++ backend/models/MeetingModel.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const MeetingSchema = new Schema ({
+    name: {
+        type: String,
+        required: [true, 'You must enter a meeting name'],
+        pattern: /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/
+    },
+    description: {
+        type: String
+    },
+    start: {
+        type: Date,
+        required: [true, 'You must provide a start date']
+    },
+    end: {
+        type: Date,
+        required: [true, 'You must provide an end date']
+    },
+    creatorID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+        required: [true, 'You must provide the meeting creator']
+    },
+    guestIDs: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student"
+    }],
+    location: {
+        type: String
+    }
+})
+
+module.exports = mongoose.model("Meeting", MeetingSchema);
