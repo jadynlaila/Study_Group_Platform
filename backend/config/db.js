b24d929108648f7a540024cdb23077a15f4fbbec
Alexander Anthis <anthisalex@gmail.com>
2024-11-15 19:20:21 -0700

    changed mongoose connect uri

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
-    try{
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`\x1b[32mUsing database '${process.env.DATABASE}'\x1b[0m`)
+
+    try {
+        const conn = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DATABASE}`);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.log(error);
