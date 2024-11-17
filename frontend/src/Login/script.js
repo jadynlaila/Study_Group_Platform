dfe57ee91efe1e3adb624cea24d8282fdeea1381
Valentino Valero <vsv7@nau.edu>
2024-11-17 14:09:01 -0700

    Added login page to frontend

---
+++ frontend/src/Login/script.js
@@ -0,0 +1,11 @@
+document.getElementById('create-account').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'flex';
+});
+
+document.getElementById('login').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('register-form').style.display = 'none';
+    document.getElementById('login-form').style.display = 'flex';
+});
