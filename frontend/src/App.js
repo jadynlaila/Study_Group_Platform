0b65e58736599943864d65d41b69a72cea8a9295
Valentino Valero <vsv7@nau.edu>
2024-11-21 16:11:25 -0700

    Changed names

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       <h1>Group Chat Application</h1>
-      <LoginPage/>
+      <Navbar/>
      {/* <TextRegion /> Doesnt need to be rendered seperatly, it is imported into GroupChatModule and loaded  dynamically */}
       {/* <GroupChatModule /> */}
     </div>
